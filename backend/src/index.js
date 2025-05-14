e4b4c3a358899b4a9ce096a0cde0e1e214b27ba7
DharaneeswarDharaneeswar-Reddy-Avula <avuladharaniswarreddy@gmail.com>
2025-05-14 14:40:24 +0530

    master

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,11 +20,17 @@ const __dirname=path.resolve();
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', MessageRoutes)
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-    })
+// if(process.env.NODE_ENV==="production"){
+//     app.use(express.static(path.join(__dirname,"../frontend/dist")));
+//     app.get("*",(req,res)=>{
+//         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+//     })
+// }
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
 }
 
 server.listen(PORT,()=>{
