0cf2655a6c01209f9f0fe104cf2932a71271415f
DharaneeswarDharaneeswar-Reddy-Avula <avuladharaniswarreddy@gmail.com>
2025-05-14 16:36:03 +0530

    Dhanu

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
-
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routers/auth.route.js";
 import messageRoutes from "./routers/message.route.js";
 import { app, server } from "./lib/socket.js";
@@ -20,7 +17,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin:["http://localhost:5173","https://chat-application-hqy4.onrender.com"],
+    origin: [
+      "http://localhost:5173",
+      "https://chat-application-hqy4.onrender.com",
+    ],
     credentials: true,
   })
 );
