368258bf0eff5369bc8a341f44d0df40b93509d4
DharaneeswarDharaneeswar-Reddy-Avula <avuladharaniswarreddy@gmail.com>
2025-05-14 16:14:54 +0530

    Dhanu

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,45 +1,42 @@
 import express from "express";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import authRoutes from "./routers/auth.route.js";
-import MessageRoutes from "./routers/message.route.js";
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 5001 // Use Render's port if available, else fallback to 5001
-app.use(express.json({ limit: "10mb" }))
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-app.use(cookieParser())
-app.use(cors({
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
     origin:["http://localhost:5173","https://chat-application-hqy4.onrender.com"],
-    credentials:true
-}))
-app.use('/api/auth', authRoutes)
-app.use('/api/messages', MessageRoutes)
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-   const __filename = fileURLToPath(import.meta.url);
-   const __dirname = dirname(__filename);
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-   // Adjust this path based on where your frontend build folder is located
-   const frontendPath = path.resolve(__dirname, "frontend", "dist");
-
-   app.use(express.static(frontendPath));
-
-   app.get("*", (req, res) => {
-      res.sendFile(path.join(frontendPath, "index.html"));
-   });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-
-
-server.listen(PORT,()=>{
-    console.log(`server is running at the port ${PORT}`);
-    connectDB();
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
