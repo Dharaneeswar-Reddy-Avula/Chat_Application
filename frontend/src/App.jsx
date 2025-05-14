b8c8788bd2ff61aae7314d43b554689cb4aed587
DharaneeswarDharaneeswar-Reddy-Avula <avuladharaniswarreddy@gmail.com>
2025-05-14 15:21:33 +0530

    Dhanu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -41,6 +41,8 @@ const App = () => {
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+        <Route path="*" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+
       </Routes>
 
       <Toaster />
